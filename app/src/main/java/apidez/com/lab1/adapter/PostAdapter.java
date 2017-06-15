abd5281a0cd66cddd7a44fe6658c0e568288b558
LÊ ĐẠI AN <ledaian41@gmail.com>
2017-06-15 22:15:11 +0700

    commit source
    completed !!

--- app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
+++ app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
@@ -2,14 +2,18 @@ package apidez.com.lab1.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
+import apidez.com.lab1.R;
 import apidez.com.lab1.model.Post;
 
 /**
@@ -17,6 +21,7 @@ import apidez.com.lab1.model.Post;
  */
 
 public class PostAdapter extends ArrayAdapter<Post> {
+    private ViewHolder viewHolder;
     private List<Post> mPosts;
 
     public PostAdapter(Context context, List<Post> posts) {
@@ -33,24 +38,57 @@ public class PostAdapter extends ArrayAdapter<Post> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // TODO: Insert your code
-        // bindViewHolder(position, viewHolder)
+//        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.item_post, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+
+            //fill data
+//            bindViewHolder(position,viewHolder);
+
+        } else {
+            convertView.getTag();
+        }
+        bindViewHolder(position, viewHolder);
         return convertView;
     }
 
     private void bindViewHolder(int position, ViewHolder viewHolder) {
         Post post = mPosts.get(position);
         // TODO: Insert your code
+        viewHolder.tvUsername.setText(post.getUsername());
+        viewHolder.tvDescription.setText(post.getDescription());
+        viewHolder.tvDate.setText(post.getDate());
+        loadImage(viewHolder.ivAvatar, post.getAvatar());
+        loadImage(viewHolder.ivImage, post.getImage());
+
     }
 
     private void loadImage(ImageView imageView, String path) {
         // TODO: Insert your code
+        Picasso.with(getContext())
+                .load(path)
+                .placeholder(R.drawable.placeholder)
+                .into(imageView);
     }
 
     public class ViewHolder {
-        public TextView tvUsername;
-        public TextView tvDescription;
-        public TextView tvDate;
-        public ImageView ivAvatar;
-        public ImageView ivImage;
+
+        public final TextView tvUsername;
+        public final TextView tvDescription;
+        public final TextView tvDate;
+        public final ImageView ivAvatar;
+        public final ImageView ivImage;
+
+        ViewHolder(View convertView) {
+            tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
+            tvDate = (TextView) convertView.findViewById(R.id.tvDate);
+            ivAvatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
+            ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+        }
     }
 }
